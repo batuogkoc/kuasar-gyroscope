b734595869fc65a6be9a89ab4047e79ec8acacf0
batuogkoc <bgunduz21@ku.edu.tr>
2025-05-24 17:58:54 +0300

    added initial files

---
+++ kuasar-gyroscope.ino
@@ -0,0 +1,73 @@
+#include <Wire.h>
+#include <Servo.h>
+#include <Adafruit_BNO055.h>
+#include <Adafruit_Sensor.h>
+
+
+// === Nesneler ===
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Servo esc;
+
+
+// === Ayarlar ===
+float setpoint = 0;       // Başlangıçta hedef roll açısı (düz konum)
+float roll_angle = 0;
+int pwm = 1500;
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+
+  if (!bno.begin()) {
+    Serial.println("BNO055 bulunamadı!");
+    while (1);
+  }
+
+
+  bno.setExtCrystalUse(true);
+
+
+  esc.attach(9, 1000, 2000);      // ESC sinyal pini
+  esc.writeMicroseconds(1500);    // motoru durdur
+  delay(2000);                    // ESC arming süresi
+}
+
+
+void loop() {
+  // Roll açısını al (x ekseni)
+  sensors_event_t orientation;
+  bno.getEvent(&orientation, Adafruit_BNO055::VECTOR_EULER);
+  roll_angle = orientation.orientation.x;
+
+
+  // Açıyı -180 ile +180 arasına düzelt
+  if (roll_angle > 180) roll_angle -= 360;
+  if (roll_angle < -180) roll_angle += 360;
+
+
+  // Hata = setpoint - mevcut açı
+  float error = setpoint - roll_angle;
+
+
+  // Basit orantılı kontrol: PWM farkı = K * hata
+  float K = 5.0;  // bu değeri sistemine göre ayarla
+  int correction = K * error;
+
+
+  // ESC PWM sinyali oluştur
+  pwm = 1500 + correction;
+  pwm = constrain(pwm, 1000, 2000);
+  esc.writeMicroseconds(pwm);
+
+
+  // Seri monitör
+  Serial.print("Roll: ");
+  Serial.print(roll_angle);
+  Serial.print(" | PWM: ");
+  Serial.println(pwm);
+
+
+  delay(20);  // ~50Hz kontrol döngüsü
+}
