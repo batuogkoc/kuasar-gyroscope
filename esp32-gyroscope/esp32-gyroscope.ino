79f12524d4b75795b27116b8f98a42f0f8890b7f
batuogkoc <bgunduz21@ku.edu.tr>
2025-05-26 18:01:14 +0300

    goddamn it works. BNO uses clock stretching (longer than default esp timeout). use i2c_set_timeout from esp-idf 5.1 (used in esp package 3.0.4) to set higher timeout to allow for clock stretching

--- esp32-gyroscope/esp32-gyroscope.ino
+++ esp32-gyroscope/esp32-gyroscope.ino
@@ -1,6 +1,13 @@
 #include "Arduino.h"
 // #include "HardwareSerial.h"
 #include <Wire.h>
+// #include "soc/soc_caps.h"
+// #include "esp32-hal-i2c.h"
+extern "C" {
+  #include "driver/i2c.h"
+  #include "driver/gpio.h"
+  #include "esp_system.h"
+}
 // #include <Adafruit_BNO055.h>
 // #include <Adafruit_Sensor.h>
 
@@ -9,15 +16,29 @@
 // Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 void setup() {
-  Serial.begin(9600);
+  delay(1000);  
+  Serial.begin(115200);
+  // Wire.begin(16, 17, 400000);
   Wire.begin();
-  Wire.setClock(400000);
-  Wire.setTimeOut(0xFFFFFFFF);
-  Wire.setTimeout(0xFFFFFFFF);
-  delay(2000);
+  int timeout=0;
+  // i2c_get_timeout(I2C_NUM_0, &timeout);
+  Serial.print("Old timeout: ");
+  Serial.println(timeout);
+  // i2c_set_timeout(I2C_NUM_0, 16000);
+  
+
+  i2c_get_timeout(I2C_NUM_0, &timeout);
+  Serial.print("New timeout: ");
+  Serial.println(timeout);
+  // Wire.setTimeOut(0xFFFFFFFF);
+  // Wire.setTimeout(0xFFFFFFFF);
+  // Wire.setTimeOut(0);
+  // Wire.setTimeout(0);
+
   Serial.println("Timeouts");
-  Serial.println(Wire.getTimeOut());
-  Serial.println(Wire.getTimeout());
+    // Serial.println(Wire.getTimeOut());
+  // Serial.println(Wire.getTimeout());
+  pinMode(19, OUTPUT);
 
 
   //  if (!bno.begin()) {
@@ -29,6 +50,7 @@ void setup() {
 
 uint8_t check_slave_connection(uint8_t address){
   Wire.beginTransmission(address);
+  // Wire.write(0);
   return Wire.endTransmission();
 }
 
@@ -36,8 +58,8 @@ uint8_t check_slave_connection(uint8_t address){
 uint8_t scan_i2c(){
   uint8_t nDevices = 0;
   for(int address = 1; address < 127; address++ ) {
-   uint8_t error = check_slave_connection(address)
-   Serial.println(error);
+   uint8_t error = check_slave_connection(address);
+  //  Serial.println(error);
    if (error == 0) {
      Serial.print("I2C device found at address 0x");
      if (address<16) {
@@ -71,8 +93,18 @@ void search_loop(){
    delay(1000);    
 }
 
-void loop() { 
+void bno_test_loop(){
+  digitalWrite(19, HIGH);
   uint8_t retval = check_slave_connection(40);
+  // delay(200);
+  digitalWrite(19, LOW);
   Serial.println(retval);
   delay(200);
+}
+
+void loop() { 
+  // search_loop();
+  check_slave_connection(0x77);
+  check_slave_connection(40);
+  delay(100);
 }
